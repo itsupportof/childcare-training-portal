498b73f4c4ff6e293a0644bad55364fec0860486
awaisOptimistic <awais@of.edu.au>
2025-01-29 11:34:53 +1100

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if(!isset($_GET['page']) && isset($_SESSION['currentSession'])){
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Bright Beginnings Family Day Care Centre</title>
-    <link rel="shortcut icon" href="https://www.brightbeginningsfdcc.com.au/wp-content/uploads/2021/04/BRIGHT-BEGINNINGS-FAMILY-DAY-CARE-Logo.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="https://andersonroadchildcare.com.au/wp-content/uploads/2024/07/Anderson-final-favicon-150x150.png" type="image/x-icon" />
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
             href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
