27e64901e0c0a63c1ca2a43946cfb18406920dcd
awaisOptimistic <awais@of.edu.au>
2025-02-05 11:53:40 +1100

    Update class.php

--- class.php
+++ class.php
@@ -812,8 +812,8 @@ class LearningHubResources{
         } catch (PDOException $e) {
             echo "Error : ".$e->getMessage();
         }
-        var_dump($row);
-        exit(0);
+        //var_dump($row);
+        //exit(0);
             echo '<a href="./?page=viewAllHubResources" class="btn btn-secondary btn-icon-split">
                                         <span class="icon text-white-50">
                                             <i class="fas fa-arrow-left"></i>
@@ -848,7 +848,7 @@ class LearningHubResources{
         </style>
         <p>
         <div class="containerIframe">
-            <iframe class="responsive-iframe" style="" src="books/index.php?location=<?php echo trim($_GET['res'])?>&amp;res=ARCCC" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true"></iframe>
+            <iframe class="responsive-iframe" style="" src="./books/index.php?location=<?php echo trim($row[0] ['res'])?>&amp;res=ARCCC" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true"></iframe>
         </div>
         </p>
         <?php
