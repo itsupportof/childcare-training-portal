db397b3ae5329cb748830d0f5158d86f10218d34
awaisOptimistic <awais@of.edu.au>
2025-02-10 14:31:25 +1100

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,8 +1,8 @@
 <?php
 /*-------------------v1.0 changes for calender-------------------------*/
- ini_set('display_errors', 1);
- ini_set('display_startup_errors', 1);
- error_reporting(1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 /*--////////////////---------v1.0 changes for calender-----------------*/
 
