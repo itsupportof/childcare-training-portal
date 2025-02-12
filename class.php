5396f4fe230c32f835a4f81dcd0cea4fb5e20a84
awaisOptimistic <awais@of.edu.au>
2025-02-12 13:36:47 +1100

    Update class.php

--- class.php
+++ class.php
@@ -782,7 +782,7 @@ class LearningHubResources{
                                 </div>
                             </div>
                             <p class="card-text" style="font-size:14px;"><?php echo $record["summary"]?></p>
-                            <a href="./?page=readHubRes&hrid=".$record["hrid"];?>" class="btn btn-primary">☝️ click here</a>
+                            <a href="./?page=readHubRes<?php echo "&hrid=".$record["hrid"];?>" class="btn btn-primary">☝️ click here</a>
                                 
                         </div>
                     </div>
@@ -1381,7 +1381,7 @@ class Resources{
                                 </div>
                             </div>
                             <p class="card-text" style="font-size:14px;"><?php echo $record["summary"]?></p>
-                            <a href="./?page=viewResource&hrid=".$record["hrid"];?>" class="btn btn-primary">☝️ click here</a>
+                            <a href="./?page=<?php echo $record["type"]."&hrid=".$record["hrid"];?>" class="btn btn-primary">☝️ click here</a>
                                 
                         </div>
                     </div>
@@ -1399,7 +1399,7 @@ class Resources{
     ///////////////////////////////////////////
     //////////Read Resources//////////////
     /////////////////////////////////////////
-    public function viewResource($hrid){
+    public function readResource($hrid){
         global $pdo;
         try {
             $query = "select * from `HubResources` where `hrid`=:hrid";
